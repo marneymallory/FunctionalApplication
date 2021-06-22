e1f3427db6b35057235bb097d0f7f016308c2587
TorchAblaze <tiffanygreathead@gmail.com>
2021-06-22 16:22:02 -0700

    Add storing state functions with html
    Co-authored-by: Marney Mallory <marney.mallory@gmail.com>
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- functionalApplication.js
+++ functionalApplication.js
@@ -20,7 +20,13 @@ const changeState = (prop) => {
     });
   };
 };
-
+let plant = { soil: 0, light: 0, water: 0 };
+let human = { water: 0, life: 5 };
 const feed = changeState("soil");
 const hydrate = changeState("water");
 const giveLight = changeState("light");
+const die = changeState("life")(-10);
+const feedByFive = feed(15);
+const waterHuman = changeState("water")(5);
+
+console.log(waterHuman(human));
